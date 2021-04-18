4989ffeb3100253bb30a4c83583822fa4f80688b
Sezana FIlipovic <sezanafilipovic@Sezanas-MacBook-Air.local>
2021-04-18 22:33:51 +0200

    Minor change

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,7 @@
 <ul>
   @foreach($posts as $post)
   <!-- <li><a href="/posts/{{$post->id}}">{{$post->title}}</a></li> -->
-  <li><a href="{{route('post', ['posts' => $post->id])}}">{{$post->title}}</a></li>
+  <li><a href="{{route('post', ['post' => $post->id])}}">{{$post->title}}</a></li>
   @endforeach
 </ul>
 @endsection
